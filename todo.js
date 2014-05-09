d0f6ef648fad350c2d6f76535d9c2755844647ca
Jamal <jmawla@yahoo.com>
2014-05-08 20:22:58 -0600

    continue coding

--- todo.js
+++ todo.js
@@ -4,14 +4,36 @@ $(document).ready(function() {
 //	var inputText = $('input').text();
 //	alert(inputText);
 
+	var func = {
+		todoInput: function() {
+
+		},
+
+		todoDblClick: function() {
+
+		},
+
+		todoChkMrk: function() {
+
+		}
+	};
+
+
+
 	function setListeners() {
 		//keystroke listener
 		var inputText = '';
-		$('#new-todo').keyup(function(event) {
+		$('#new-todo').keyup(function(e) {
 			inputText = $(this).val();
 			console.log(inputText);
-			if(event.which === 13) {
-				alert(inputText);
+			if(e.which === 13) {
+				$('#todo-list').append("<li></li>");
+				$('#todo-list li').append("<div class=\"view\"></div>");
+				$('#todo-list li .view').append("<input class=\"toggle\" type=\"checkbox\">");
+				$('#todo-list li .view').append("<label></label>");
+				$('#todo-list li .view label').text(inputText);
+				$('#todo-list li .view').append("<button class=\"destroy\"></button>");
+				$('#new-todo').val('');
 			}
 		});
 
@@ -23,7 +45,14 @@ $(document).ready(function() {
 		})
 
 
+		//checkmark click listener
+		//completed all click
+		//remove all completed
+		//delete
+		//hover
+		//all/active/completed
 	};
 
 	setListeners();
+
 });
